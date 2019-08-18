5a4e9a4b7365405d9ead4c0021fbdace5a63886b
Mateusz Samsel <msamsel@users.noreply.github.com>
2019-08-18 16:18:28 +0200

    Make register resiliant to negative values.

--- src/vmgeneralpurposeregister.js
+++ src/vmgeneralpurposeregister.js
@@ -1,5 +1,18 @@
-export default class VMGeneralPurposeRegister {
-	constructor() {
-		this.value = 0;
+const _value = Symbol( 'value' );
+
+export default class VMGeneralPurposeRegister {}
+
+Object.defineProperty( VMGeneralPurposeRegister.prototype, 'value', {
+	get: function() {
+		return this[ _value ];
+	},
+
+	set: function( val ) {
+		// Fix negative numbers;
+		if ( val < 0 ) {
+			val += 0xffffffff + 1;
+		}
+
+		this[ _value ] = val;
 	}
-}
+} );
